f629492323b527f0a72758c3297b6dbdd53f1fb4
Danielle Robitshek <drobi710@gmail.com>
2021-04-06 13:26:26 -0400

    added a new message button that displays user contacts

--- data/Users.ts
+++ data/Users.ts
@@ -11,53 +11,62 @@ export default [
     name: "Lukas",
     imageUri:
       "https://upload.wikimedia.org/wikipedia/commons/3/3e/Lukas_Rieger_%28Red_carpet%29_-_Global_Citizen_Festival_Hamburg_02.jpg",
+    status: "Hello there, let's chat",
   },
   {
     id: "u3",
     name: "Daniil",
     imageUri:
       "https://www.atptour.com/-/media/tennis/players/head-shot/2020/medvedev_head_ao20.png",
+    status: "I'm new here",
   },
   {
     id: "u4",
     name: "Alex",
     imageUri:
       "https://www.mckinsey.com/~/media/McKinsey/Our%20People/Alex%20Sawaya/Alex-Sawaya_2310_profile_1536x1152.jpg",
+    status: "Hello there, how are you",
   },
   {
     id: "u5",
     name: "Vlad",
     imageUri:
       "https://content.fortune.com/wp-content/uploads/2020/11/GettyImages-1185353508_web.jpg",
+    status: "Hello there, how are you",
   },
   {
     id: "u6",
     name: "Elon Musk",
     imageUri:
       "https://static.theceomagazine.net/wp-content/uploads/2018/10/15093202/elon-musk.jpg",
+    status: "I'm the best",
   },
   {
     id: "u7",
     name: "Adrian",
     imageUri:
       "https://static.wikia.nocookie.net/liverpoolfc/images/9/99/Adrian2020.jpeg/revision/latest/scale-to-width-down/340?cb=20200803041822",
+    status: "Hello there, how are you",
   },
   {
     id: "u8",
     name: "Borja",
     imageUri:
       "https://resources.premierleague.com/premierleague/photos/players/250x250/p83091.png",
+    status: "Hello there, how are you",
   },
   {
     id: "u9",
     name: "Mom",
     imageUri:
       "https://wwwimage-tve.cbsstatic.com/thumbnails/photos/w400-q80/cast/mimikennedy.jpg",
+    status: "Hello there, how are you",
   },
   {
     id: "u10",
     name: "Angelina Jolie",
     imageUri:
       "https://upload.wikimedia.org/wikipedia/commons/a/ad/Angelina_Jolie_2_June_2014_%28cropped%29.jpg",
+    status: "I kick butts",
   },
 ];
